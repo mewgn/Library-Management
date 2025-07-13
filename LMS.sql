6cc7888d38e2461b56298820c30b3b2371a2f197
Louella <pltagui@mymail.mapua.edu.ph>
2025-07-13 19:20:27 +0800

    Add files via upload

---
+++ LMS.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE LibrarySystem;
+USE LibrarySystem;
+
+CREATE DATABASE IF NOT EXISTS LibrarySystem;
+USE LibrarySystem;
+
+CREATE TABLE User (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    fullname VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    role VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY AUTO_INCREMENT,
+    UserID INT NOT NULL,
+    StudentNumber VARCHAR(11) UNIQUE NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES User(id)
+);
+
+CREATE TABLE Librarian (
+    LibrarianID INT PRIMARY KEY AUTO_INCREMENT,
+    UserID INT NOT NULL,
+    EmployeeNumber VARCHAR(11) UNIQUE NOT NULL,
+    FOREIGN KEY (UserID) REFERENCES User(id)
+);
+
+CREATE TABLE Category (
+    CategoryID INT PRIMARY KEY AUTO_INCREMENT,
+    CategoryName VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE LibraryMaterial (
+    BookCode VARCHAR(20) PRIMARY KEY,
+    BookName VARCHAR(255) NOT NULL,
+    Authors VARCHAR(255),
+    PublishedDate DATE,
+    CategoryID INT,
+    Publisher VARCHAR(100),
+    ISBN VARCHAR(20),
+    Status VARCHAR(20) DEFAULT 'Available',
+    FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID)
+);
+
+CREATE TABLE BorrowingList (
+    BorrowID INT PRIMARY KEY AUTO_INCREMENT,
+    StudentID INT,
+    BookCode VARCHAR(20),
+    BorrowedDate DATE,
+    ExpectedReturnDate DATE,
+    ActualReturnDate DATE,
+    ReturnStatus VARCHAR(20) DEFAULT 'Returned',
+    FOREIGN KEY (StudentID) REFERENCES Student(StudentID),
+    FOREIGN KEY (BookCode) REFERENCES LibraryMaterial(BookCode)
+);
